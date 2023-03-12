d9916a6625e14274bda23096d0320da6d16bf7b0
Ilya Yalymov <chezeee@gmail.com>
2023-03-12 16:16:04 +0300

    Initial structure added

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+/* index.js */
+const checkForNewNotifications = () => {
+  // TODO
+};
+
+// do NOT modify this function
+function showNewNotifications(count) {
+  console.log(
+    `You have ${count} new notifications. Would you like to read them?`
+  );
+}
+
+// Sample usage - do not modify
+checkForNewNotifications();
